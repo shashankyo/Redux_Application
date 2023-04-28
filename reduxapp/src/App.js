ecf64ef59adca4e52897946c57f6087ac7f2979f
shashank <shashank944533@gmail.com>
2023-04-28 16:07:43 +0530

    Redux App Cart Balance

---
+++ reduxapp/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import  {Navbar } from './Navbar';
+import { Shop } from './Shop';
+
+function App() {
+  return (
+    <>
+      <Navbar/>
+
+    <div className="container text-center">
+      <Shop/>
+    </div>
+      
+
+    </>
+  );
+}
+
+export default App;
---
+++ reduxapp/src/Shop.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const Shop = () => {
+  return (
+    <div>
+        <h2>Buy Adidas Shoes 50 Rs</h2>
+        <button className="btn btn-primary mx-2">-</button>
+        Add this item to Cart
+        <button className="btn btn-primary mx-2">+</button>
+
+    </div>
+  )
+}
